1569f33f034a7cb49469642fb8ba6948e2b9fa2e
Kálmán Szalai - KAMI <kami911@gmail.com>
2015-05-13 16:10:49 +0200

    Updated first version that generates grid for correcting grid

--- gridcreator.py
+++ gridcreator.py
@@ -10,18 +10,39 @@ def frange(start, stop, step):
     yield r
     r += step
 
-with open('wgs84longlat-hun.txt', 'w') as wgslltxtfile:
-  with open('wgs84geo-hun.txt', 'w') as wgsgctxtfile:
-    with open('eovhun.txt', 'w') as eovtxtfile:
-      for fi in frange (45.7800, 48.6000, 0.1):
-        for la in frange (16.1200,  22.9100, 0.1):
-          for he in range (100, 200, 10):
-            print ('O: %s %s %s' % (fi, la, he))
-            wgslltxtfile.write('%s %s %s\r\n' % (fi, la, he))
-            XProjected, YProjected,ZProjected = transform(WGS84, WGS84Geo, la, fi, he)
-            print ('P: %s, %s, %s' % (XProjected, YProjected, ZProjected))
-            wgsgctxtfile.write('PONT  %s  %s  %s\r\n' % (XProjected, YProjected, ZProjected))
-            XEOVProjected, YEOVProjected, ZEOVProjected = transform(WGS84Geo, EOV, XProjected, YProjected, ZProjected)
-            print ('E: %s, %s, %s' % (XEOVProjected, YEOVProjected, ZEOVProjected))
-            eovtxtfile.write('%s  %s  %s\r\n' % (XEOVProjected, YEOVProjected, ZEOVProjected))
+with open('plain-wgs84longlat-hun.txt', 'w') as wgs_ll_txtfile:
+  with open('plain-wgs84geo-hun.txt', 'w') as wgs_gc_txtfile:
+    with open('point-wgs84geo-hun.txt', 'w') as point_wgs_gc_txtfile:
+      with open('ppac-wgs84geo-hun', 'w') as ppac_wgs_gc_txtfile:
+        with open('plain-eov-hun.txt', 'w') as eov_txtfile:
+          with open('point-eov-hun.txt', 'w') as point_eov_txtfile:
+            with open('ppac-eov-hun', 'w') as ppac_eov_txtfile:
+              index = 1
+              ppac_wgs_gc_txtfile.write('Time[s],X[m],Y[m],Z[m],Roll[deg],Pitch[deg],Yaw[deg]\r\n')
+              ppac_eov_txtfile.write('Time[s],X[m],Y[m],Z[m],Roll[deg],Pitch[deg],Yaw[deg]\r\n')
+              # Budapest:
+              # 47.3900, 47.5700, 0.001
+              # 18.9300,  19.1900, 0.001
+              # Hungary:
+              # 45.7800, 48.6000, 0.01
+              # 16.1200,  22.9100, 0.01
+              for fi in frange (47.3900, 47.5700, 0.001):
+                for la in frange (18.9300,  19.1900, 0.001):  
+                  for he in range (100, 101, 1):
+                    ## WGS84 longlat
+                    print ('O: %.8f, %.8f, %.8f' % (la, fi, he))
+                    wgs_ll_txtfile.write('%.8f %.8f %.8f\n' % (la, fi, he))
+                    ## WGS84 geocent
+                    XProjected, YProjected,ZProjected = transform(WGS84, WGS84Geo, la, fi, he)
+                    print ('P: %.8f, %.8f, %.8f' % (XProjected, YProjected, ZProjected))
+                    wgs_gc_txtfile.write('%.8f  %.8f  %.8f\n' % (XProjected, YProjected, ZProjected))
+                    point_wgs_gc_txtfile.write('PONT              %.3f      %.3f      %.3f\n' % (XProjected, YProjected, ZProjected))
+                    ppac_wgs_gc_txtfile.write('%s,%.14f,%.14f,%.14f,1,1,1\n' % (index, XProjected, YProjected, ZProjected))
+                    ## EOV
+                    XEOVProjected, YEOVProjected, ZEOVProjected = transform(WGS84, EOV, la, fi, he)
+                    print ('E: %.8f, %.8f, %.8f' % (XEOVProjected, YEOVProjected, ZEOVProjected))
+                    eov_txtfile.write('%.8f  %.8f  %.8f\n' % (XEOVProjected, YEOVProjected, ZEOVProjected))
+                    point_eov_txtfile.write('PONT              %.3f      %.3f      %.3f\n' % (XEOVProjected, YEOVProjected, ZEOVProjected))
+                    ppac_eov_txtfile.write('%s,%.14f,%.14f,%.14f,1,1,1\n' % (index, XEOVProjected, YEOVProjected, ZEOVProjected))
+                    index = index + 1                
 
