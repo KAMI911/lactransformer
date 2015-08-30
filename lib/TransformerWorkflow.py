01f1d64b09b3b3b7f766a2c23e1964c3449f99c4
KAMI <kami911@gmail.com>
2015-08-30 19:36:47 +0200

    Use function for input_format friendly names
    Refactoring TransformerWorkflow to use newly created FriendlyName that returns a human readable names of specific input formats.

--- lib/TransformerWorkflow.py
+++ lib/TransformerWorkflow.py
@@ -1,7 +1,7 @@
 try:
     import logging
     import multiprocessing
-    from lib import LasPyConverter, TxtPyConverter
+    from lib import LasPyConverter, TxtPyConverter, FriendlyName
 except ImportError as err:
     print("Error import module: " + str(err))
     exit(128)
@@ -19,12 +19,14 @@ def Transformer(parameters):
     # Get name for this process
     current = multiprocessing.current_process()
     proc_name = current.name
-
+    # Define friendly name of input formats
+    input_format_name = FriendlyName.FriendlyName(input_format)
     logging.info('[%s] Starting ...' % (proc_name))
     if input_format in ['las', 'laz']:
         logging.info(
-            '[%s] Opening %s LAS PointCloud file for converting to %s LAS PointCloud file ... Source projections is: "%s", destination projection is: "%s".' % (
-                proc_name, source_file, destination_file, source_projection, destination_projection))
+            '[%s] Opening %s %s file for converting to %s %s file ... Source projections is: "%s", destination projection is: "%s".' % (
+                proc_name, source_file, input_format_name, destination_file, input_format_name, source_projection,
+                destination_projection))
         # Opening source LAS files for read and write
         try:
             lasFiles = LasPyConverter.LasPyConverter(source_file, source_projection, destination_file,
@@ -34,11 +36,11 @@ def Transformer(parameters):
             logging.error('Cannot open files: %s and %s, error: %s.' % (source_file, destination_file, str(err)))
             exit(10)
         try:
-            logging.info('[%s] Scaling LAS PointCloud.' % (proc_name))
+            logging.info('[%s] Scaling %s.' % (proc_name, input_format_name))
             lasFiles.GetSourceScale()
             trX, trY, trZ = lasFiles.SetDestinationScale()
-            logging.info('[%s] LAS PointCloud file transformed offset: %s %s %s' % (proc_name, trX, trY, trZ))
-            logging.info('[%s] Transforming LAS PointCloud.' % (proc_name))
+            logging.info('[%s] %s file transformed offset: %s %s %s' % (proc_name, input_format_name, trX, trY, trZ))
+            logging.info('[%s] Transforming %s.' % (proc_name, input_format_name))
             lasFiles.TransformPointCloud()
         except Exception as err:
             logging.error(
@@ -46,25 +48,31 @@ def Transformer(parameters):
             exit(11)
         else:
             logging.info(
-                '[%s] Successfully transformed LAS PointCloud data for file: %s.' % (proc_name, destination_file))
+                '[%s] Successfully transformed %s data for file: %s.' % (
+                    proc_name, input_format_name, destination_file))
         try:
-            logging.info('[%s] Closing transformed %s LAS PointCloud file.' % (proc_name, destination_file))
+            logging.info('[%s] Closing transformed %s %s file.' % (proc_name, destination_file, input_format_name))
             lasFiles.Close(full_header_update)
         except Exception as err:
             logging.error('Cannot close files: %s and %s, error: %s.' % (source_file, destination_file, str(err)))
             exit(12)
         else:
-            logging.info('[%s] Transformed %s LAS PointCloud file has created.' % (proc_name, destination_file))
+            logging.info('[%s] Transformed %s %s file has created.' % (proc_name, destination_file, input_format_name))
             return 0
 
-    elif input_format == 'txt' or input_format == 'lastxt':
+    elif input_format in ['txt', 'lastxt', 'iml', 'csv']:
         logging.info(
-            '[%s] Opening %s PointText file for converting to %s PointText file ... Source projections is: "%s", destination projection is: "%s".' % (
-                proc_name, source_file, destination_file, source_projection, destination_projection))
+            '[%s] Opening %s %s file for converting to %s %s file ... Source projections is: "%s", destination projection is: "%s".' % (
+                proc_name, source_file, input_format_name, destination_file, input_format_name, source_projection,
+                destination_projection))
         # Opening source LAS files for read and write
         try:
-            txtFiles = TxtPyConverter.TxtPyConverter(source_file, source_projection, destination_file,
-                                                     destination_projection, txt_separator)
+            if input_format in ['txt', 'lastxt']:
+                txtFiles = TxtPyConverter.TxtPyConverter(source_file, source_projection, destination_file,
+                                                         destination_projection, txt_separator)
+            if input_format in ['iml', 'csv']:
+                txtFiles = TxtPyConverter.TxtPyConverter(source_file, source_projection, destination_file,
+                                                         destination_projection)
             txtFiles.Open()
         except Exception as err:
             logging.error('Cannot open file: %s.' % (str(err)))
@@ -74,81 +82,23 @@ def Transformer(parameters):
                 txtFiles.TransformPointText()
             elif input_format == 'lastxt':
                 txtFiles.TransformLASText()
-        except Exception as err:
-            logging.error(
-                'Cannot transform files form %s to %s, error: %s.' % (source_file, destination_file, str(err)))
-            exit(11)
-        else:
-            logging.info('[%s] Successfully transformed PointText data for file: %s.' % (proc_name, destination_file))
-        try:
-            logging.info('[%s] Closing transformed %s PointText file.' % (proc_name, destination_file))
-            txtFiles.Close()
-        except Exception as err:
-            logging.error('Cannot close files: %s and %s, error: %s.' % (source_file, destination_file, str(err)))
-            exit(12)
-        else:
-            logging.info('[%s] Transformed %s PointText file has created.' % (proc_name, destination_file))
-            return 0
-
-    elif input_format == 'iml':
-
-        logging.info(
-            '[%s] Opening %s TerraPhoto Image List file for converting to %s TerraPhoto Image List file ... Source projections is: "%s", destination projection is: "%s".' % (
-                proc_name, source_file, destination_file, source_projection, destination_projection))
-        # Opening source LAS files for read and write
-        try:
-            txtFiles = TxtPyConverter.TxtPyConverter(source_file, source_projection, destination_file,
-                                                     destination_projection)
-            txtFiles.Open()
-        except Exception as err:
-            logging.error('Cannot open file: %s.' % (str(err)))
-            exit(10)
-        try:
-            txtFiles.TransformPointIML()
-        except Exception as err:
-            logging.error(
-                'Cannot transform files form %s to %s, error: %s.' % (source_file, destination_file, str(err)))
-            exit(11)
-        else:
-            logging.info('[%s] Successfully transformed PointText data for file: %s.' % (proc_name, destination_file))
-        try:
-            logging.info('[%s] Closing transformed %s TerraPhoto Image List file.' % (proc_name, destination_file))
-            txtFiles.Close()
-        except Exception as err:
-            logging.error('Cannot close files: %s and %s, error: %s.' % (source_file, destination_file, str(err)))
-            exit(12)
-        else:
-            logging.info('[%s] Transformed %s TerraPhoto Image List file has created.' % (proc_name, destination_file))
-            return 0
-
-    elif input_format == 'csv':
-
-        logging.info(
-            '[%s] Opening %s Riegl Camera CSV file for converting to %s Riegl Camera CSV file ... Source projections is: "%s", destination projection is: "%s".' % (
-                proc_name, source_file, destination_file, source_projection, destination_projection))
-        # Opening source LAS files for read and write
-        try:
-            txtFiles = TxtPyConverter.TxtPyConverter(source_file, source_projection, destination_file,
-                                                     destination_projection)
-            txtFiles.Open()
-        except Exception as err:
-            logging.error('Cannot open file: %s.' % (str(err)))
-            exit(10)
-        try:
-            txtFiles.TransformPointCSV()
+            elif input_format == 'iml':
+                txtFiles.TransformPointIML()
+            elif input_format == 'csv':
+                txtFiles.TransformPointCSV()
         except Exception as err:
             logging.error(
                 'Cannot transform files form %s to %s, error: %s.' % (source_file, destination_file, str(err)))
             exit(11)
         else:
             logging.info(
-                '[%s] Successfully transformed  Riegl Camera CSV data for file: %s.' % (proc_name, destination_file))
+                '[%s] Successfully transformed %s for file: %s.' % (proc_name, input_format_name, destination_file))
         try:
-            logging.info('[%s] Closing transformed %s Riegl Camera CSV file.' % (proc_name, destination_file))
+            logging.info('[%s] Closing transformed %s %s file.' % (proc_name, destination_file, input_format_name))
             txtFiles.Close()
         except Exception as err:
             logging.error('Cannot close files: %s and %s, error: %s.' % (source_file, destination_file, str(err)))
             exit(12)
         else:
-            logging.info('[%s] Transformed %s Riegl Camera CSV file has created.' % (proc_name, destination_file))
+            logging.info('[%s] Transformed %s %s file has created.' % (proc_name, destination_file, input_format_name))
             return 0
