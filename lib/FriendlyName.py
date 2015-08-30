a8b92ad8318f86877986de8bfc9911e8363d2d2e
Kálmán Szalai - KAMI <kami911@gmail.com>
2015-08-30 19:35:10 +0200

    Use function for input_format friendly names
    Refactoring TransformerWorkflow to use newly created FriendlyName that returns a human readable names of specific input formats.

---
+++ lib/FriendlyName.py
@@ -0,0 +1,21 @@
+try:
+    import logging
+except ImportError as err:
+    print("Error import module: " + str(err))
+    exit(128)
+
+
+def FriendlyName(input_format):
+    # This function returns with friendly name of selected input format
+    if input_format == 'las':
+        return 'LAS PointCloud'
+    elif input_format == 'laz':
+        return 'LAZ PointCloud'
+    elif input_format == 'txt':
+        return 'PointText'
+    elif input_format == 'lastxt':
+        return 'PointText'
+    elif input_format == 'iml':
+        return 'TerraPhoto Image List'
+    elif input_format == 'csv':
+        return 'Riegl Camera CSV'
