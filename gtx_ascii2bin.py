de71fb84b2048d8b64f08be8175401a502293298
Kálmán Szalai - KAMI <kami911@gmail.com>
2016-06-09 09:24:14 +0200

    Add gtx file format converters * bin2ASCII and ASCII2bin

---
+++ gtx_ascii2bin.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+
+import struct
+
+import os
+from libs import FileConverterCommandLine
+
+# It is big endian
+# Documentation: http://vdatum.noaa.gov/docs/gtx_info.html
+HEADER_STRUCT = '>4d2i'
+DATA_STRUCT = '>f'
+
+fileconverterworkflow = FileConverterCommandLine.FileConverterCommandLine()
+fileconverterworkflow.parse()
+
+input_file = fileconverterworkflow.get_input()
+output_file = fileconverterworkflow.get_output()
+
+index = 0
+
+if output_file is None:
+    output_file = input_file + '.gtx'
+
+if not os.path.isfile(input_file):
+    raise Exception('Cannot found input file: {0}'.format(input_file))
+else:
+    try:
+        with open(output_file, 'w') as fw:
+            with open(input_file, 'r') as fr:
+                row = fr.readline()
+                row = row.strip().split(' ')
+                header = struct.pack(HEADER_STRUCT, float(row[0]), float(row[1]), float(row[2]), float(row[3]),
+                                     int(row[4]), int(row[5]))
+                fw.write(header)
+                # print(header, end='')
+                while 1:
+                    try:
+                        height = fr.readline()
+                        height = height.strip()
+                        # Insert special null value, when the input uses the base level (no geoid offset for this point)
+                        if height == '150':
+                            height = '-88.8888'
+                        data = struct.pack(DATA_STRUCT, float(height.strip()))
+                        print(height)
+                        fw.write(data)
+                        index += 1
+                    except ValueError as err:
+                        print (err)
+                        print('Number of lines: {0} position {2} value: W{1}W'.format(index, height, fr.tell()))
+                        break
+
+    except IOError as err:
+        print('Error opening: {0}, error: {1}'.format(output_file, err))
+        exit(1)
---
+++ gtx_bin2ascii.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import os
+import struct
+from libs import FileConverterCommandLine
+
+# It is big endian
+# Documentation: http://vdatum.noaa.gov/docs/gtx_info.html
+HEADER_STRUCT = '>4d2i'
+DATA_STRUCT = '>f'
+
+fileconverterworkflow = FileConverterCommandLine.FileConverterCommandLine()
+fileconverterworkflow.parse()
+
+index = 0
+
+input_file = fileconverterworkflow.get_input()
+output_file = fileconverterworkflow.get_output()
+
+if output_file is None:
+    output_file = input_file + '.txt'
+
+if not os.path.isfile(input_file):
+    raise Exception('Cannot found input file: {0}'.format(input_file))
+else:
+    try:
+        with open(output_file, 'w') as fw:
+            with open (input_file, 'rb') as fr:
+                row = fr.read(40)
+                # I cannot really understand but it seems it is big endian
+                # Documentation: http://vdatum.noaa.gov/docs/gtx_info.html
+                header = struct.unpack(HEADER_STRUCT, row)
+                line = ' '.join(map(str,header))+'\n'
+                fw.write(line)
+                print(line, end='')
+                while 1:
+                    try:
+                        height = fr.read(4)
+                        if len(height) == 4:
+                            data = struct.unpack(DATA_STRUCT, height)
+                            line = '{0:.4f}\n'.format(data[0])
+                            # print(line, end='')
+                            fw.write(line)
+                            # print('Number of lines: {0} position {2} value: W{1}W'.format(index, height, fr.tell()))
+                            index += 1
+                        else:
+                            break
+                    except ValueError as err:
+                        print('Number of lines: {0} position {2} value: W{1}W'.format(index, height, fr.tell()))
+                        break
+    except IOError as err:
+        print ('Error opening: {0}, error: {1}'.format(output_file, err))
+        exit(1)
