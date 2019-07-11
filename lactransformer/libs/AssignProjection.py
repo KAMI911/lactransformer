f23ea699f3ac084e6fb7cf26925e398fcf119872
Kálmán „KAMI” Szalai <kami911@gmail.com>
2019-07-11 15:27:56 +0200

    Move projection strings to separated place

--- lactransformer/libs/AssignProjection.py
+++ lactransformer/libs/AssignProjection.py
@@ -26,6 +26,18 @@ def AssignProjectionString(projection, proc_name = 'Unknown'):
     geoidgrids_EOV2014fine = grid_path('geoid_eht2014_fine.gtx')
     geoidgrids_SVY21c = grid_path('geoid_svy21_2009.gtx')
 
+    projections = {'WGS84': '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',
+                   'WGS84geo': '+proj=geocent +ellps=WGS84 +datum=WGS84 +units=m +no_defs',
+                   'EOV': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +units=m +no_defs',
+                   'EOVc': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +nadgrids=' + nadgrids_EOV2014 + ' +geoidgrids=' + geoidgrids_EOV2014 + ' +units=m +no_defs',
+                   'EOV2014': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +nadgrids=' + nadgrids_EOV2014 + ' +geoidgrids=' + geoidgrids_EOV2014 + ' +units=m +no_defs',
+                   'EOV2014fine': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +nadgrids=' + nadgrids_EOV2014 + ' +geoidgrids=' + geoidgrids_EOV2014fine + ' +units=m +no_defs',
+                   'EOV2009': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +nadgrids=' + nadgrids_EOV2009 + ' +geoidgrids=' + geoidgrids_EOV2009 + ' +units=m +no_defs',
+                   'SVY21': '+proj=tmerc +lat_0=1.366666666666667 +lon_0=103.8333333333333 +k=1 +x_0=28001.642 +y_0=38744.572 +ellps=WGS84 +units=m +no_defs',
+                   'SVY21c': '+proj=tmerc +lat_0=1.366666666666667 +lon_0=103.8333333333333 +k=1 +x_0=28001.642 +y_0=38744.572 +ellps=WGS84 +geoidgrids=' + geoidgrids_SVY21c + ' +units=m +no_defs',
+                   'ETRS89': '+proj=longlat +ellps=GRS80 +no_defs',
+                   'ETRS89geo': '+proj=geocent +ellps=GRS80 +units=m +no_defs'}
+
     if projection in ['EOVc', 'EOV2014']:
         if os.path.isfile(nadgrids_EOV2014) and os.path.isfile(geoidgrids_EOV2014):
             logging.info('[{0}] Found all required grids ...'.format(proc_name))
@@ -56,8 +68,8 @@ def AssignProjectionString(projection, proc_name = 'Unknown'):
         else:
             logging.error('Cannot found %s grid.' % (geoidgrids_SVY21c))
             exit(2)
-    if projection in supported_projections.projections:
-        return supported_projections.projections[projection]
+    if projection in projections:
+        return projections[projection]
     else:
         return False
 
@@ -67,8 +79,20 @@ def AssignFallbackProjectionString(projection):
     # WGS84 = Proj(init='EPSG:4326')
     # WGS84Geo = Proj(init='EPSG:4328')
 
-    if projection in supported_projections.fallback_projections:
-        return supported_projections.fallback_projections[projection]
+    fallback_projections = {'WGS84': '',
+                            'WGS84geo': '',
+                            'EOV': '',
+                            'EOVc': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +units=m +no_defs',
+                            'EOV2014': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +units=m +no_defs',
+                            'EOV2014fine': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +units=m +no_defs',
+                            'EOV2009': '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +units=m +no_defs',
+                            'SVY21': '',
+                            'SVY21c': '+proj=tmerc +lat_0=1.366666666666667 +lon_0=103.8333333333333 +k=1 +x_0=28001.642 +y_0=38744.572 +ellps=WGS84 +units=m +no_defs',
+                            'ETRS89': '',
+                            'ETRS89geo': ''}
+
+    if projection in fallback_projections:
+        return fallback_projections[projection]
     else:
         return False
 
